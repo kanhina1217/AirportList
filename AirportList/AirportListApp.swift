e1b8313add3d2cb0f27d47f929beb8b462a5ac16
kanhina1217 <kanhina1217@gmail.com>
2025-07-31 19:31:39 +0900

    Initial Commit

---
+++ AirportList/AirportListApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirportListApp.swift
+//  AirportList
+//
+//  Created by Kyoko Hobo on 2025/07/31.
+//
+
+import SwiftUI
+
+@main
+struct AirportListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirportListTests/AirportListTests.swift
@@ -0,0 +1,17 @@
+//
+//  AirportListTests.swift
+//  AirportListTests
+//
+//  Created by Kyoko Hobo on 2025/07/31.
+//
+
+import Testing
+@testable import AirportList
+
+struct AirportListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
